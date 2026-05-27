08a6c6792978332be1c2ed3a091b1a16e49c4890
Sarah Boucher <tcgtcg0132@outlook.fr>
2026-05-27 16:41:31 -0400

    Changer listeEmprunts de ArrayList<Livre> a ArrayList<Emprunt>

--- src/Usager.java
+++ src/Usager.java
@@ -3,5 +3,5 @@ import java.util.ArrayList;
 public abstract class Usager {
     private String nom;
     private int ID;
-    private ArrayList<Livre> listeEmprunts;
+    private ArrayList<Emprunt> listeEmprunts;
 }
